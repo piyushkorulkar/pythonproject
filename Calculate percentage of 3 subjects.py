7293e47bb02aa3e3be961975357fa31303ce12cc
piyushkorulkar <piyushkorulkar2304@gmail.com>
2026-08-13 15:11:46 +0530

    Add files via upload

---
+++ Calculate percentage of 3 subjects.py
@@ -0,0 +1,7 @@
+subject1 = float(input("Enter marks of Subject 1: "))
+subject2 = float(input("Enter marks of Subject 2: "))
+subject3 = float(input("Enter marks of Subject 3: "))
+total = subject1 + subject2 + subject3
+percentage = (total / 300) * 100
+print("Total Marks =", total)
+print("Percentage =", percentage, "%")
